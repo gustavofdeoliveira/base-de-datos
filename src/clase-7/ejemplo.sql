84c3bb2ce756d045687ee6c9c6d2602475986323
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-09-24 19:30:51 -0300

    feat: ejemplo selects

--- src/clase-7/ejemplo.sql
+++ src/clase-7/ejemplo.sql
@@ -18,3 +18,37 @@ where f2006_estadios.nombre like '%h%'
 ## Filtrar por nombre de estadio que tenga la letra h
 select * from f2006_estadios
 where lower(nombre) like '%h%'
+
+## Transformar el id_estadio a entero y filtrar por los estadios cuyo id_estadio sea 0, 1, 2 o 3
+select * from f2006_estadios
+where cast(id_estadio as integer) between 0 and 3
+
+## ordenar por id_estadio de forma descendente
+select * from f2006_arbitros
+order by id_arbitro desc
+
+## seleccionar el maximo de estatura
+select max(estatura)
+from f2006_arbitros
+
+select nombre, estatura from f2006_arbitros
+where estatura = (
+select max(estatura)
+from f2006_arbitros
+)
+order by estatura desc
+
+select * from f2006_arbitros
+where nacionalidad like 'M_xico'
+and estatura = (
+    select max(estatura)
+    from f2006_arbitros
+    where nacionalidad like 'M_xico'
+) 
+
+
+SELECT nombre, estatura, id_arbitro, (
+    SELECT count(id_partido)
+    FROM f2006_partidos as x
+    WHERE x.id_arbitro = a.id_arbitro) as num_partidos
+FROM f2006_arbitros a
